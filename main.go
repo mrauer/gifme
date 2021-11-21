3fd56029db044f049c219a6c90fad7526c35b4c5
Maxime Rauer <maxime.rauer@gmail.com>
2021-11-21 11:24:57 -0600

    [MID] Full Logic

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/mrauer/gifme/lib"
+	"github.com/xfrr/goffmpeg/transcoder"
+)
+
+const (
+	INPUT_PATH   = "example/input.mp4"
 )
 
 func main() {
-	fmt.Println("main")
+	trans := new(transcoder.Transcoder)
 
-	fmt.Println(lib.FMT_LIB)
+	err := lib.PaletteGen(trans, INPUT_PATH)
+
+	if err == nil {
+		err = lib.PaletteUse(INPUT_PATH)
+	}
+
+	if err == nil {
+		lib.CompressGit()
+	}
+
+	return
 }
